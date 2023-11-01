ee2277a9f05aa7a3edcd012f2d3b7123f28e1ce4
Hung-Yun-Ching <144878766+Hung-Yun-Ching@users.noreply.github.com>
2023-11-01 15:01:19 +0800

    Add files via upload

---
+++ 11:1.R
@@ -0,0 +1,79 @@
+flights = readRDS("data/week7/flights.rds")
+
+data2 <- list(
+  meta = list(
+    name="航空公司統一代碼",
+    source_link ="https://data.gov.tw/dataset/8088"
+  ),
+  file = "data/week7/airlines.json"
+)
+
+flights$data[[2]] <- data2
+
+airlines <-
+  jsonlite::fromJSON(
+    flights$data[[2]][["file"]]
+  )
+
+flights$data[[2]][["data_frame"]] <- airlines
+
+saveRDS(flights, file="flights.rds")
+
+dplyr::left_join(
+  flightsData, airlines,
+  by="AirlineID"
+) -> flightsData
+
+
+
+
+
+airportr::airports ->airports
+
+head(airports$Country)
+airports$Country == "Taiwan"
+
+airports[pick_taiwan, ] |> View()
+
+airports$IATA[pick_taiwan]
+
+airports |>
+dplyr::fliter(
+  Country == "Taiwan"
+) -> airports_taiwan
+
+taiwan_IATA <- unique(airports_taiwan$IATA)
+
+
+pick_departure <- flightsData$DepartureAirportID %in% taiwan_IATA
+head(pick_departure)
+flightsData[pick_departure, ] |> View()
+
+which(pick_departure) |> head(10)
+
+whichIsDeparture <- which(pick_departure)
+flightsData[whichIsDeparture, ]
+
+flightsData |>
+  dplyr::filter(
+    DepartureAirportID %in% unique(airports_taiwan[["IATA"]])) -> 
+  departure_flightsData
+
+flightsData |>
+  dplyr::filter(
+    ArrivalAirportID %in% unique(airports_taiwan[["IATA"]])) -> 
+  arrival_flightsData
+
+
+flights$data[[3]] <- list(
+  departure_flightsData= departure_flightsData,
+  arrival_flightsData = arrival_flightsData
+)
+
+
+
+
+
+
+
+
