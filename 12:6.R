1fe50b295c5a58c1dd76c362db34b3a83e25d5f9
Hung-Yun-Ching <144878766+Hung-Yun-Ching@users.noreply.github.com>
2023-12-06 13:37:30 +0800

    Add files via upload

---
+++ 12:6.R
@@ -0,0 +1,66 @@
+# a block of program
+# code back
+
+# example of a code black
+# programming block
+{
+  # code lines
+}
+
+# use it repetitively
+# add for (...) in front of it 
+for (.x in ...)
+{
+  #code lines
+}
+
+
+{
+   x+3
+}
+
+
+values_s <- c(2, 4, 7, 15)
+seq_along(values_x)
+for(x in seq_along(values_X))
+{
+  values_[[x]]+3
+}  
+
+
+#Task by situation
+
+oneComingStudentMajor <- sample(c("Economics", "Sociology"), 1)
+print(oneComingStudentMajor)
+
+#code block 1
+if (oneComingStudentMajor == "Economics" ) #condition inside()
+{
+  # oneComingStudentMajor == "Economics"
+print("Please go to room 201.")
+}
+
+#code block 2
+if (oneComingStudentMajor == "Sociology") #condition inside()
+{
+  # oneComingStudentMajor == "Sociology"
+print("Please go to room 301.")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
